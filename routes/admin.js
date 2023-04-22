b9e792386bf7bc761a5d03da518580b703976187
Simon Ragara <simonotienoragara2000@gmail.com>
2023-04-22 01:12:21 -0700

    fixed the image paths logic

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const storageConfig = multer.diskStorage({
         cb(null, 'images')
     },
     filename: function(req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
+        cb(null, file.originalname)
     }
 })
 
